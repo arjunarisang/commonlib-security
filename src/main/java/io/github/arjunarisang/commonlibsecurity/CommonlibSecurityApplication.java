95eba01c1f73392a646e2bb0111e4e66bca43cad
Arjuna Risang <arjunarisang@gmail.com>
2021-01-12 15:13:46 +0700

    first blood

---
+++ src/main/java/io/github/arjunarisang/commonlibsecurity/CommonlibSecurityApplication.java
@@ -0,0 +1,13 @@
+package io.github.arjunarisang.commonlibsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonlibSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonlibSecurityApplication.class, args);
+	}
+
+}
